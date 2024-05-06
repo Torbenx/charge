7b08ec44b8c72d4c416d117e955091d098d67ab9
Torben Thaysen <thaysentorben@gmail.com>
2024-05-06 21:51:41 +0200

    small bump alloc

--- compiler/PageBumpAllocator.h
+++ compiler/PageBumpAllocator.h
@@ -16,34 +16,34 @@ struct PageBumpAllocator {
     static constexpr int_t DEFAULT_CAPACITY = 2ll * 1024ll * 1024ll * 1024ll / sizeof(T);
 
     PageBumpAllocator(int_t capacity = DEFAULT_CAPACITY) {
-        m_begin = (T*)allocdetail::reserveMemory(capacity * sizeof(T));
-        offset = m_begin;
-        capacityEnd = m_begin + capacity;
+        offsetBytes = 0;
+        capacityBytes = capacity * sizeof(T);
+        m_begin = (T*)allocdetail::reserveMemory(capacityBytes);
         allocdetail::commitMemory(m_begin);
     }
 
     T* begin() { return m_begin; }
     const T* begin() const { return m_begin; }
-    T* end() { return offset; }
-    const T* end() const { return offset; }
+    T* end() { return ptrFromBytes(offsetBytes); }
+    const T* end() const { return ptrFromBytes(offsetBytes); }
     T* data() { return m_begin; }
     const T* data() const { return m_begin; }
     T& front() { return *m_begin; }
     const T& front() const { return *m_begin; }
-    T& back() { return *(offset - 1); }
-    const T& back() const { return *(offset - 1); }
+    T& back() { return *ptrFromBytes(offsetBytes - sizeof(T)); }
+    const T& back() const { return *ptrFromBytes(offsetBytes - sizeof(T)); }
     T& operator[](int_t index) { return m_begin[index]; }
     const T& operator[](int_t index) const { return m_begin[index]; }
-    int_t size() const { return offset - m_begin; }
-    int_t capacity() const { return capacityEnd - m_begin; }
+    int_t size() const { return offsetBytes / sizeof(T); }
+    int_t capacity() const { return capacityBytes / sizeof(T); }
 
     T* allocate() {
-        T* oldOffset = offset;
-        offset += 1;
-        if (((uintptr_t)oldOffset ^ (uintptr_t)offset) >= allocdetail::COMMIT_GRANULARITY) {
+        uint32_t oldOffsetBytes = offsetBytes;
+        offsetBytes += sizeof(T);
+        if ((oldOffsetBytes ^ offsetBytes) >= allocdetail::COMMIT_GRANULARITY) {
             grow();
         }
-        return oldOffset;
+        return ptrFromBytes(oldOffsetBytes);
     }
 
     void push_back(T t) {
@@ -52,22 +52,26 @@ struct PageBumpAllocator {
     }
 
     void clear() {
-        std::destroy(m_begin, offset);
-        offset = m_begin;
+        std::destroy_n(data(), size());
+        offsetBytes = 0;
     }
 
     ~PageBumpAllocator() {
-        std::destroy(m_begin, offset);
-        allocdetail::releaseMemory(m_begin, capacity() * sizeof(T));
+        std::destroy_n(data(), size());
+        allocdetail::releaseMemory(data(), capacity() * sizeof(T));
     }
 
 private:
     T* m_begin = nullptr;
-    T* offset = nullptr;
-    T* capacityEnd = nullptr;
+    uint32_t offsetBytes = 0;
+    uint32_t capacityBytes = 0;
 
-    [[gnu::noinline]] void grow() {
-        VERIFY(offset <= capacityEnd);
-        allocdetail::commitMemory((void*)((uintptr_t)offset & ~((uintptr_t)allocdetail::COMMIT_GRANULARITY - 1)));
+    T* ptrFromBytes(uint32_t bytes) const {
+        return reinterpret_cast<T*>(reinterpret_cast<std::byte*>(m_begin) + bytes);
+    }
+
+    void grow() {
+        VERIFY(offsetBytes <= capacityBytes);
+        allocdetail::commitMemory(ptrFromBytes(offsetBytes & ~(allocdetail::COMMIT_GRANULARITY - 1)));
     }
 };
