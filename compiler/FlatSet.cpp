d96f842d5565aa5d4cc07058d79db417226067a1
Torben Thaysen <thaysentorben@gmail.com>
2024-10-10 21:12:39 +0200

    Finish FlatSet

--- compiler/FlatSet.cpp
+++ compiler/FlatSet.cpp
@@ -1,1 +1,89 @@
 #include <FlatSet.h>
+
+#include <gtest/gtest.h>
+
+namespace {
+[[maybe_unused]] void dumpSet(const FlatSet<int>& set) {
+    std::string s;
+    for (int i : set) {
+        if (!s.empty())
+            s += ", ";
+        s += std::to_string(i);
+    }
+    fmt::println("{}", s);
+}
+}
+
+TEST(FlatSet, Basic) {
+    FlatSet<int> empty;
+    EXPECT_EQ(empty.size(), 0);
+    EXPECT_EQ(empty.capacity(), 0);
+
+    auto set123 = FlatSet<int>::fromSorted(std::array { 1, 2, 3 });
+    EXPECT_EQ(set123.size(), 3);
+    EXPECT_GE(set123.capacity(), 3);
+
+    auto set = set123;
+    set.unionWith(empty);
+    EXPECT_EQ(set, set123);
+    set.unionWith(empty);
+    EXPECT_EQ(set, set123);
+    set.unionWith(empty);
+    EXPECT_EQ(set, set123);
+    set.unionWith(empty);
+    EXPECT_EQ(set, set123);
+
+    set.unionWith(set123);
+    EXPECT_EQ(set, set123);
+    set.unionWith(set123);
+    EXPECT_EQ(set, set123);
+    set.unionWith(set123);
+    EXPECT_EQ(set, set123);
+    set.unionWith(set123);
+    EXPECT_EQ(set, set123);
+
+    auto set456 = FlatSet<int>::fromSorted(std::array { 4, 5, 6 });
+    EXPECT_EQ(set456.size(), 3);
+    EXPECT_GE(set456.capacity(), 3);
+
+    auto set123456 = FlatSet<int>::fromSorted(std::array { 1, 2, 3, 4, 5, 6 });
+    EXPECT_EQ(set123456.size(), 6);
+    EXPECT_GE(set123456.capacity(), 6);
+
+    set.unionWith(set456);
+    EXPECT_EQ(set, set123456);
+    set.unionWith(set123);
+    EXPECT_EQ(set, set123456);
+
+    set.intersectionWith(set123);
+    EXPECT_EQ(set, set123);
+    set.intersectionWith(set123);
+    EXPECT_EQ(set, set123);
+    set.intersectionWith(set456);
+    EXPECT_EQ(set, empty);
+
+    set.intersectionWith(set123456);
+    EXPECT_EQ(set, empty);
+    set.unionWith(set123456);
+    EXPECT_EQ(set, set123456);
+    set.intersectionWith(empty);
+    EXPECT_EQ(set, empty);
+
+    auto set135 = FlatSet<int>::fromSorted(std::array { 1, 3, 5 });
+    EXPECT_EQ(set135.size(), 3);
+    EXPECT_GE(set135.capacity(), 3);
+
+    auto set246 = FlatSet<int>::fromSorted(std::array { 2, 4, 6 });
+    EXPECT_EQ(set246.size(), 3);
+    EXPECT_GE(set246.capacity(), 3);
+
+    set = set123456;
+    set.intersectionWith(set135);
+    EXPECT_EQ(set, set135);
+    set.intersectionWith(set246);
+    EXPECT_EQ(set, empty);
+    set.unionWith(set246);
+    EXPECT_EQ(set, set246);
+    set.unionWith(set135);
+    EXPECT_EQ(set, set123456);
+}
--- compiler/FlatSet.h
+++ compiler/FlatSet.h
@@ -3,30 +3,56 @@
 #include <types.h>
 
 template<typename T>
+struct OperatorOrdering {
+    std::strong_ordering operator()(const T& a, const T& b) const { return a <=> b; }
+};
+
+template<typename T, typename Compare = OperatorOrdering<T>, typename... Args>
 struct FlatSet {
-    void unionWith(const FlatSet& other) {
+    static FlatSet unionOf(Args... args, const FlatSet& a, const FlatSet& b) {
+        FlatSet result;
+        result.s = Storage(a.size() + b.size());
+        result.s.end = forwardUnion(args..., result.s.begin, a.s.span(), b.s.span());
+        return result;
+    }
+
+    static FlatSet intersectionOf(Args... args, const FlatSet& a, const FlatSet& b) {
+        FlatSet result;
+        result.s = Storage(std::min(a.size(), b.size()));
+        result.s.end = intersection(args..., result.s.begin, a.s.span(), b.s.span());
+        return result;
+    }
+
+    void unionWith(Args... args, const FlatSet& other) {
         if (s.capacity < s.begin) {
             if (s.begin - s.capacity < other.size()) {
-                Storage newStorage = Storage(other.size() + size());
-                newStorage.end = forwardUnion(newStorage.span(), s.span(), other.s.span());
-                s = std::move(newStorage);
+                *this = unionOf(args..., *this, other);
             } else {
                 T* capacityEnd = s.end;
-                s.end = forwardUnion({ s.capacity, capacityEnd }, s.span(), other.s.span());
+                s.end = forwardUnion(args..., s.capacity, s.span(), other.s.span());
                 s.begin = s.capacity;
                 s.capacity = capacityEnd;
             }
         } else {
             if (s.capacity - s.end < other.size()) {
-                Storage newStorage = Storage(other.size() + size());
-                newStorage.end = forwardUnion(newStorage.span(), s.span(), other.s.span());
-                s = std::move(newStorage);
+                *this = unionOf(args..., *this, other);
             } else {
                 T* capacityBegin = s.begin;
-                s.begin = backwardUnion({ capacityBegin, s.capacity }, s.span(), other.s.span()) + 1;
-                s.end = s.capcity;
+                s.begin = backwardUnion(args..., s.capacity - 1, s.span(), other.s.span()) + 1;
+                s.end = s.capacity;
                 s.capacity = capacityBegin;
             }
+        }
+    }
+
+    void intersectionWith(Args... args, const FlatSet& other) {
+        if (s.capacity < s.begin) {
+            T* capacityEnd = s.end;
+            s.end = intersection(args..., s.capacity, s.span(), other.s.span());
+            s.begin = s.capacity;
+            s.capacity = capacityEnd;
+        } else {
+            s.end = intersection(args..., s.begin, s.span(), other.s.span());
         }
     }
 
@@ -37,7 +63,31 @@ struct FlatSet {
             return s.capacity - s.begin;
     }
 
-    int_t size() const { return s.begin - s.end; }
+    int_t size() const { return s.end - s.begin; }
+
+    const T* begin() const { return s.begin; }
+    const T* end() const { return s.end; }
+
+    bool operator==(const FlatSet& other) const {
+        if (size() != other.size())
+            return false;
+        return std::equal(s.begin, s.end, other.s.begin);
+    }
+
+    FlatSet() = default;
+    FlatSet(const FlatSet& other)
+        : FlatSet(other.s.span()) { }
+    FlatSet& operator=(const FlatSet& other) {
+        *this = FlatSet(other.s.span());
+        return *this;
+    }
+    FlatSet(FlatSet&& other) = default;
+    FlatSet& operator=(FlatSet&&) = default;
+    ~FlatSet() = default;
+
+    static FlatSet fromSorted(std::span<const T> sorted) {
+        return FlatSet(sorted);
+    }
 
 private:
     struct Members {
@@ -49,7 +99,12 @@ private:
     };
     struct Storage : Members {
         Storage() = default;
-        Storage(int_t requiredSize);
+        Storage(int_t requiredSize) {
+            std::allocator<T> alloc;
+            size_t size = std::bit_ceil<size_t>(requiredSize);
+            this->begin = this->end = alloc.allocate(size);
+            this->capacity = this->begin + size;
+        }
         Storage(const Storage&) = delete;
         Storage& operator=(const Storage&) = delete;
         Storage(Storage&& other)
@@ -60,6 +115,7 @@ private:
             deallocate();
             (Members&)* this = other;
             (Members&)other = {};
+            return *this;
         }
         void deallocate() {
             std::allocator<T> alloc;
@@ -74,18 +130,38 @@ private:
         }
     };
 
-    Storage storageFor(int_t size) {
-        if (s.capacity < s.begin) {
-            if (s.end - s.capacity >= size)
-                return std::move(*this);
-            return Storage(size);
-        }
+    explicit FlatSet(std::span<const T> sorted) {
+        s = Storage(sorted.size());
+        s.end = std::copy(sorted.begin(), sorted.end(), s.begin);
     }
 
-    static T* forwardUnion(std::span<T> target, std::span<const T> a, std::span<const T> b) {
+    static T* intersection(Args... args, T* insert, std::span<const T> a, std::span<const T> b) {
         auto aIt = a.begin();
         auto bIt = b.begin();
-        auto insert = target.begin();
+        Compare comp;
+        for (;;) {
+            if (aIt == a.end() || bIt == b.end())
+                break;
+
+            auto ordering = comp(args..., *aIt, *bIt);
+            if (ordering < 0) {
+                ++aIt;
+            } else if (ordering > 0) {
+                ++bIt;
+            } else {
+                *insert = *aIt;
+                ++insert;
+                ++aIt;
+                ++bIt;
+            }
+        }
+        return insert;
+    }
+
+    static T* forwardUnion(Args... args, T* insert, std::span<const T> a, std::span<const T> b) {
+        auto aIt = a.begin();
+        auto bIt = b.begin();
+        Compare comp;
         for (;;) {
             if (aIt == a.end()) {
                 insert = std::copy(bIt, b.end(), insert);
@@ -95,10 +171,11 @@ private:
                 insert = std::copy(aIt, a.end(), insert);
                 break;
             }
-            if (*aIt < *bIt) {
+            auto ordering = comp(args..., *aIt, *bIt);
+            if (ordering < 0) {
                 *insert = *aIt;
                 ++aIt;
-            } else if (*bIt < aIt) {
+            } else if (ordering > 0) {
                 *insert = *bIt;
                 ++bIt;
             } else {
@@ -108,26 +185,28 @@ private:
             }
             ++insert;
         }
-        return &*insert;
+        return insert;
     }
 
-    static T* backwardUnion(std::span<T> target, std::span<const T> a, std::span<const T> b) {
+    static T* backwardUnion(Args... args, T* target, std::span<const T> a, std::span<const T> b) {
         auto aIt = a.rbegin();
         auto bIt = b.rbegin();
-        auto insert = target.rbegin();
+        auto insert = std::span<T>(target, 1).rbegin();
+        Compare comp;
         for (;;) {
-            if (aIt == a.end()) {
-                insert = std::copy(bIt, b.end(), insert);
+            if (aIt == a.rend()) {
+                insert = std::copy(bIt, b.rend(), insert);
                 break;
             }
-            if (bIt == b.end()) {
-                insert = std::copy(aIt, a.end(), insert);
+            if (bIt == b.rend()) {
+                insert = std::copy(aIt, a.rend(), insert);
                 break;
             }
-            if (*aIt > *bIt) {
+            auto ordering = comp(args..., *aIt, *bIt);
+            if (ordering > 0) {
                 *insert = *aIt;
                 ++aIt;
-            } else if (*bIt > aIt) {
+            } else if (ordering < 0) {
                 *insert = *bIt;
                 ++bIt;
             } else {
